4df53eebe25b35187b714bfe2e503e15660ff4b2
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-08-26 06:22:41 +0300

    Lecture 6 - Creating Fonts Manager

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Montserrat";
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+}
