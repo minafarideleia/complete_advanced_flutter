041982d16e0d7f1fba7ef5fa7028684b4be585e0
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-08-26 05:49:18 +0300

    Lecture 5 - Creating Colors Manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = HexColor.fromHex("#ED9728");
+  static Color darkGrey = HexColor.fromHex("#525252");
+  static Color grey = HexColor.fromHex("#737477");
+  static Color lightGrey = HexColor.fromHex("#9E9E9E");
+  static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = "FF" + hexColorString; // 8 char with opacity 100%
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
