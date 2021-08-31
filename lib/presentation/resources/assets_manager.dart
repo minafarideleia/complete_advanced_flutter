bc2f026a994cf79756b97c12077d7ae7bab5d7e2
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-08-31 06:53:25 +0300

    Lecture 29 - Implementation Onboarding Screen Part4

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -6,4 +6,8 @@ class ImageAssets {
   static const String onboardingLogo2 = "$IMAGE_PATH/onboarding_logo2.svg";
   static const String onboardingLogo3 = "$IMAGE_PATH/onboarding_logo3.svg";
   static const String onboardingLogo4 = "$IMAGE_PATH/onboarding_logo4.svg";
+  static const String hollowCircleIc = "$IMAGE_PATH/hollow_cirlce_ic.svg";
+  static const String leftArrowIc = "$IMAGE_PATH/left_arrow_ic.svg";
+  static const String rightarrowIc = "$IMAGE_PATH/right_arrow_ic.svg";
+  static const String solidCircleIc = "$IMAGE_PATH/solid_circle_ic.svg";
 }
