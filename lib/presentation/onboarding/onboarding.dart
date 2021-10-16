1a7fcfbd16711ae52c884fe026ece23feb0566b7
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-17 01:51:13 +0300

    Lecture 38 Onboarding Viewmodel Logic Implemenation

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -17,20 +17,7 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
-  late final List<SliderObject> _list = _getSliderData();
   PageController _pageController = PageController(initialPage: 0);
-  int _currentIndex = 0;
-
-  List<SliderObject> _getSliderData() => [
-        SliderObject(AppStrings.onBoardingSubTitle1,
-            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
-        SliderObject(AppStrings.onBoardingSubTitle2,
-            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
-        SliderObject(AppStrings.onBoardingSubTitle3,
-            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
-        SliderObject(AppStrings.onBoardingSubTitle4,
-            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4)
-      ];
 
   @override
   Widget build(BuildContext context) {
@@ -161,6 +148,12 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     } else {
       return SvgPicture.asset(ImageAssets.solidCircleIc); // unselected slider
     }
+  }
+
+  @override
+  void dispose() {
+    // TODO: viewmodel.dispose();
+    super.dispose();
   }
 }
 
