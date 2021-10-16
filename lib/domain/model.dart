d83d4306fea8fc67b107590d03ac36cc85f04c6b
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-17 01:20:07 +0300

    Lecture 37 Adding Stream Controller to Onboarding Viewmodel

---
+++ lib/domain/model.dart
@@ -0,0 +1,7 @@
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
+}
--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -200,11 +200,3 @@ class OnBoardingPage extends StatelessWidget {
     );
   }
 }
-
-class SliderObject {
-  String title;
-  String subTitle;
-  String image;
-
-  SliderObject(this.title, this.subTitle, this.image);
-}
