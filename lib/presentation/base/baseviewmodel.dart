7d59b251935c0db45885f8d5ffafb1755b683a28
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-17 00:30:05 +0300

    Lecture 34 Creating Base ViewModel

---
+++ lib/presentation/base/baseviewmodel.dart
@@ -0,0 +1,10 @@
+abstract class BaseViewModel {
+  // shared variables and functions that will be used through any view model.
+}
+
+abstract class BaseViewModelInputs {
+  void start(); // will be called while init. of view model
+  void dispose(); // will be called when viewmodel dies.
+}
+
+abstract class BaseViewModelOutputs {}
