7959286583fab46dcf3885e7d953caaa93fecb49
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-20 01:58:35 +0300

    Lecture 50 Data Layer Creating Mappers and Applying Null Safety

--- lib/app/extensions.dart
+++ lib/app/extensions.dart
@@ -1,9 +1,11 @@
 // extension on String
 
+import 'package:complete_advanced_flutter/data/mapper/mapper.dart';
+
 extension NonNullString on String? {
   String orEmpty() {
     if (this == null) {
-      return "";
+      return EMPTY;
     } else {
       return this!;
     }
@@ -15,7 +17,7 @@ extension NonNullString on String? {
 extension NonNullInteger on int? {
   int orZero() {
     if (this == null) {
-      return 0;
+      return ZERO;
     } else {
       return this!;
     }
