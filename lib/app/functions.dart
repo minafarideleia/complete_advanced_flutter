ee81f6c8d58507c582ee1687c324910f2b1baccd
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-22 15:06:59 +0300

    Lecture 70 Domain Layer Getting Device Info Part2

--- lib/app/functions.dart
+++ lib/app/functions.dart
@@ -14,12 +14,20 @@ Future<DeviceInfo> getDeviceDetails() async {
   try {
     if (Platform.isAndroid) {
       // return android device info
+      var build = await deviceInfoPlugin.androidInfo;
+      name = build.brand + " " + build.model;
+      identifier = build.androidId;
+      version = build.version.codename;
     } else if (Platform.isIOS) {
       // return ios device info
+      var build = await deviceInfoPlugin.iosInfo;
+      name = build.name + " " + build.model;
+      identifier = build.identifierForVendor;
+      version = build.systemVersion;
     }
   } on PlatformException {
     // return default data here
-    return DeviceInfo(name, identifier, version)
+    return DeviceInfo(name, identifier, version);
   }
-  return DeviceInfo(name, identifier, version)
+  return DeviceInfo(name, identifier, version);
 }
