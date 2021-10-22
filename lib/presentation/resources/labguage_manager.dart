a2a3cee36919383e78767392a081abca5aee9df9
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-22 13:37:35 +0300

    Lecture 64 Presentation Layer Adding Language Manager

---
+++ lib/presentation/resources/labguage_manager.dart
@@ -0,0 +1,15 @@
+enum LanguageType { ENGLISH, ARABIC }
+
+const String ARABIC = "ar";
+const String ENGLISH = "en";
+
+extension LanguageTypeExtension on LanguageType {
+  String getValue() {
+    switch (this) {
+      case LanguageType.ENGLISH:
+        return ENGLISH;
+      case LanguageType.ARABIC:
+        return ARABIC;
+    }
+  }
+}
