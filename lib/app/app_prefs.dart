07bfec28418c9d0f05f2590e928cbc2b5003dbf5
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-22 13:57:51 +0300

    Lecture 65 Application Layer Adding Application Preferences

---
+++ lib/app/app_prefs.dart
@@ -0,0 +1,20 @@
+import 'package:complete_advanced_flutter/presentation/resources/language_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String PREFS_KEY_LANG = "PREFS_KEY_LANG";
+
+class AppPreferences {
+  SharedPreferences _sharedPreferences;
+
+  AppPreferences(this._sharedPreferences);
+
+  Future<String> getAppLanguage() async {
+    String? language = _sharedPreferences.getString(PREFS_KEY_LANG);
+
+    if (language != null && language.isNotEmpty) {
+      return language;
+    } else {
+      return LanguageType.ENGLISH.getValue();
+    }
+  }
+}
