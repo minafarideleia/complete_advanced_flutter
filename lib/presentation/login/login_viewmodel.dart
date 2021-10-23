ca7782d83218af933d735fe79473d90093d2e045
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-23 22:22:00 +0300

    Lecture 72 Adding Streams to Login ViewModel

--- lib/presentation/login/login_viewmodel.dart
+++ lib/presentation/login/login_viewmodel.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+
 import 'package:complete_advanced_flutter/presentation/base/baseviewmodel.dart';
 
 class LoginViewModel extends BaseViewModel
     with LoginViewModelInputs, LoginViewModelOutputs {
+  StreamController _userNameStreamController =
+      StreamController<String>.broadcast();
+  StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
   // inputs
   @override
   void dispose() {
-    // TODO: implement dispose
+    _userNameStreamController.close();
+    _passwordStreamController.close();
   }
 
   @override
@@ -14,12 +22,10 @@ class LoginViewModel extends BaseViewModel
   }
 
   @override
-  // TODO: implement inputPassword
-  Sink get inputPassword => throw UnimplementedError();
+  Sink get inputPassword => _passwordStreamController.sink;
 
   @override
-  // TODO: implement inputUserName
-  Sink get inputUserName => throw UnimplementedError();
+  Sink get inputUserName => _userNameStreamController.sink;
 
   @override
   login() {
@@ -41,12 +47,22 @@ class LoginViewModel extends BaseViewModel
 
   // outputs
   @override
-  // TODO: implement outputIsPasswordValid
-  Stream<bool> get outputIsPasswordValid => throw UnimplementedError();
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
 
   @override
-  // TODO: implement outputIsUserNameValid
-  Stream<bool> get outputIsUserNameValid => throw UnimplementedError();
+  Stream<bool> get outputIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
+
+  // private functions
+
+  bool _isPasswordValid(String password) {
+    return password.isNotEmpty;
+  }
+
+  bool _isUserNameValid(String userName) {
+    return userName.isNotEmpty;
+  }
 }
 
 abstract class LoginViewModelInputs {
