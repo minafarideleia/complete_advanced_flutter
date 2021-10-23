44b7af1c1a5be473ab477cd64545360435950f8a
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-23 23:05:45 +0300

    Lecture 73 Adding Data Classes to Login ViewModel

---
+++ lib/presentation/common/freezed_data_classes.freezed.dart
@@ -0,0 +1,162 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'freezed_data_classes.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$LoginObjectTearOff {
+  const _$LoginObjectTearOff();
+
+  _LoginObject call(String userName, String password) {
+    return _LoginObject(
+      userName,
+      password,
+    );
+  }
+}
+
+/// @nodoc
+const $LoginObject = _$LoginObjectTearOff();
+
+/// @nodoc
+mixin _$LoginObject {
+  String get userName => throw _privateConstructorUsedError;
+  String get password => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $LoginObjectCopyWith<LoginObject> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LoginObjectCopyWith<$Res> {
+  factory $LoginObjectCopyWith(
+          LoginObject value, $Res Function(LoginObject) then) =
+      _$LoginObjectCopyWithImpl<$Res>;
+  $Res call({String userName, String password});
+}
+
+/// @nodoc
+class _$LoginObjectCopyWithImpl<$Res> implements $LoginObjectCopyWith<$Res> {
+  _$LoginObjectCopyWithImpl(this._value, this._then);
+
+  final LoginObject _value;
+  // ignore: unused_field
+  final $Res Function(LoginObject) _then;
+
+  @override
+  $Res call({
+    Object? userName = freezed,
+    Object? password = freezed,
+  }) {
+    return _then(_value.copyWith(
+      userName: userName == freezed
+          ? _value.userName
+          : userName // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$LoginObjectCopyWith<$Res>
+    implements $LoginObjectCopyWith<$Res> {
+  factory _$LoginObjectCopyWith(
+          _LoginObject value, $Res Function(_LoginObject) then) =
+      __$LoginObjectCopyWithImpl<$Res>;
+  @override
+  $Res call({String userName, String password});
+}
+
+/// @nodoc
+class __$LoginObjectCopyWithImpl<$Res> extends _$LoginObjectCopyWithImpl<$Res>
+    implements _$LoginObjectCopyWith<$Res> {
+  __$LoginObjectCopyWithImpl(
+      _LoginObject _value, $Res Function(_LoginObject) _then)
+      : super(_value, (v) => _then(v as _LoginObject));
+
+  @override
+  _LoginObject get _value => super._value as _LoginObject;
+
+  @override
+  $Res call({
+    Object? userName = freezed,
+    Object? password = freezed,
+  }) {
+    return _then(_LoginObject(
+      userName == freezed
+          ? _value.userName
+          : userName // ignore: cast_nullable_to_non_nullable
+              as String,
+      password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_LoginObject implements _LoginObject {
+  _$_LoginObject(this.userName, this.password);
+
+  @override
+  final String userName;
+  @override
+  final String password;
+
+  @override
+  String toString() {
+    return 'LoginObject(userName: $userName, password: $password)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _LoginObject &&
+            (identical(other.userName, userName) ||
+                const DeepCollectionEquality()
+                    .equals(other.userName, userName)) &&
+            (identical(other.password, password) ||
+                const DeepCollectionEquality()
+                    .equals(other.password, password)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(userName) ^
+      const DeepCollectionEquality().hash(password);
+
+  @JsonKey(ignore: true)
+  @override
+  _$LoginObjectCopyWith<_LoginObject> get copyWith =>
+      __$LoginObjectCopyWithImpl<_LoginObject>(this, _$identity);
+}
+
+abstract class _LoginObject implements LoginObject {
+  factory _LoginObject(String userName, String password) = _$_LoginObject;
+
+  @override
+  String get userName => throw _privateConstructorUsedError;
+  @override
+  String get password => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$LoginObjectCopyWith<_LoginObject> get copyWith =>
+      throw _privateConstructorUsedError;
+}
