6cc51c69b9f8f7bb27a08e78a0026c528b6f70d2
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-24 22:25:44 +0300

    Lecture 78 Login View UI Continue

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -14,4 +14,11 @@ class AppStrings {
   static const String onBoardingSubTitle4 =
       "Tut app is an awesome flutter application using clean architecture #4";
   static const String skip = "Skip";
+  static const String username = "Username";
+  static const String usernameError = "Username is wrong";
+  static const String passwordError = "password is wrong";
+  static const String password = "Password";
+  static const String login = "Login";
+  static const String forgetPassword = "Forget password";
+  static const String registerText = "Not a member ? Sign up";
 }
