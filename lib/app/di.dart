a1402f48a390ed7f77b80bb3dc6ff67d3eea9110
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-25 01:50:22 +0300

    Lecture 83 Adding Dependency Injection App Module

---
+++ lib/app/di.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'app_prefs.dart';
+
+final instance = GetIt.instance;
+
+Future<void> initAppModule() async {
+  final sharedPrefs = await SharedPreferences.getInstance();
+
+  // shared prefs instance
+  instance.registerLazySingleton<SharedPreferences>(() => sharedPrefs);
+
+  // app prefs instance
+  instance
+      .registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+}
