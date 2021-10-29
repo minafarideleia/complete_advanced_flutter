6188b44b798bc9ecd81381259f418ca7682d3750
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-10-29 12:19:47 +0300

    Lecture 85 Add Dependency Injection Login Module

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -4,6 +4,8 @@ import 'package:complete_advanced_flutter/data/network/dio_factory.dart';
 import 'package:complete_advanced_flutter/data/network/network_info.dart';
 import 'package:complete_advanced_flutter/data/repository/repository_impl.dart';
 import 'package:complete_advanced_flutter/domain/repository/repository.dart';
+import 'package:complete_advanced_flutter/domain/usecase/login_usecase.dart';
+import 'package:complete_advanced_flutter/presentation/login/login_viewmodel.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -41,3 +43,17 @@ Future<void> initAppModule() async {
   instance.registerLazySingleton<Repository>(
       () => RepositoryImpl(instance(), instance()));
 }
+
+initLoginModule(){
+  if(!GetIt.I.isRegistered<LoginUseCase>()){
+    instance.registerFactory<LoginUseCase>(() => LoginUseCase(instance()));
+    instance.registerFactory<LoginViewModel>(() => LoginViewModel(instance()));
+  }
+}
+
+
+
+
+
+
+
