41b440bfebd016307831740a96f692a729d64993
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-01 02:58:28 +0300

    lecture 104 Adding Input Stream Controller Inside Base ViewModel

--- lib/presentation/base/baseviewmodel.dart
+++ lib/presentation/base/baseviewmodel.dart
@@ -1,11 +1,34 @@
+import 'dart:async';
+
+import 'package:complete_advanced_flutter/presentation/common/state_renderer/state_render_impl.dart';
+
 abstract class BaseViewModel extends BaseViewModelInputs
     with BaseViewModelOutputs {
-  // shared variables and functions that will be used through any view model.
+  StreamController _inputStateStreamController =
+      StreamController<FlowState>.broadcast();
+
+  @override
+  Sink get inputState => _inputStateStreamController.sink;
+
+  @override
+  Stream<FlowState> get outputState =>
+      _inputStateStreamController.stream.map((flowState) => flowState);
+
+  @override
+  void dispose() {
+    _inputStateStreamController.close();
+  }
+
+// shared variables and functions that will be used through any view model.
 }
 
 abstract class BaseViewModelInputs {
   void start(); // will be called while init. of view model
   void dispose(); // will be called when viewmodel dies.
+
+  Sink get inputState;
 }
 
-abstract class BaseViewModelOutputs {}
+abstract class BaseViewModelOutputs {
+  Stream<FlowState> get outputState;
+}
