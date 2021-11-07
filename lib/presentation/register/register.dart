3802bf8236983786e0f39b81899bfc5361934cf9
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-07 03:07:09 +0300

    Lecture 129 Binding Register ViewModel to View and Adding Dependencey Injection

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -1,3 +1,5 @@
+import 'package:complete_advanced_flutter/app/di.dart';
+import 'package:complete_advanced_flutter/presentation/register/register_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatefulWidget {
@@ -8,6 +10,42 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  RegisterViewModel _viewModel = instance<RegisterViewModel>();
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController _userNameTextEditingController =
+      TextEditingController();
+  TextEditingController _mobileNumberTextEditingController =
+      TextEditingController();
+  TextEditingController _userEmailEditingController = TextEditingController();
+  TextEditingController _userPasswordEditingController =
+      TextEditingController();
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  _bind() {
+    _viewModel.start();
+    _userNameTextEditingController.addListener(() {
+      _viewModel.setUserName(_userNameTextEditingController.text);
+    });
+
+    _userPasswordEditingController.addListener(() {
+      _viewModel.setPassword(_userPasswordEditingController.text);
+    });
+
+    _userEmailEditingController.addListener(() {
+      _viewModel.setEmail(_userEmailEditingController.text);
+    });
+
+    _mobileNumberTextEditingController.addListener(() {
+      _viewModel.setMobileNumber(_mobileNumberTextEditingController.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
@@ -15,7 +53,7 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _viewModel.dispose();
     super.dispose();
   }
 }
