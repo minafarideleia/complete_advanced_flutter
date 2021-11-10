90d77df2b28dd67ae693a4c40eb9e187f81cb5ae
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-10 03:35:27 +0300

    Lecture 148 Adding Mappers for Home APIs part2

--- lib/data/mapper/mapper.dart
+++ lib/data/mapper/mapper.dart
@@ -57,3 +57,28 @@ extension BannerResponseMapper on BannerResponse? {
         this?.image?.orEmpty() ?? EMPTY, this?.link?.orEmpty() ?? EMPTY);
   }
 }
+
+extension HomeResponseMapper on HomeResponse? {
+  HomeObject toDomain() {
+    List<Service> mappedServices =
+        (this?.data?.services?.map((service) => service.toDomain()) ??
+                Iterable.empty())
+            .cast<Service>()
+            .toList();
+
+    List<Store> mappedStores =
+        (this?.data?.stores?.map((store) => store.toDomain()) ??
+                Iterable.empty())
+            .cast<Store>()
+            .toList();
+
+    List<BannerAd> mappedBanners =
+        (this?.data?.stores?.map((bannerAd) => bannerAd.toDomain()) ??
+                Iterable.empty())
+            .cast<BannerAd>()
+            .toList();
+
+    var data = HomeData(mappedServices, mappedStores, mappedBanners);
+    return HomeObject(data);
+  }
+}
