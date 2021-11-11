6bf6994828d57eb39a45444709a678113e369988
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-12 02:16:03 +0300

    Lecture 156 Home View UI

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:complete_advanced_flutter/app/di.dart';
+import 'package:complete_advanced_flutter/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:complete_advanced_flutter/presentation/main/home/home_viewmodel.dart';
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,8 +28,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(AppStrings.home),
+      child: SingleChildScrollView(
+        child: StreamBuilder<FlowState>(
+          stream: _viewModel.outputState,
+          builder: (context, snapshot) {
+            return snapshot.data?.getScreenWidget(context, _getContentWidgets(),
+                    () {
+                  _viewModel.start();
+                }) ??
+                Container();
+          },
+        ),
+      ),
     );
+  }
+
+  Widget _getContentWidgets() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _getBannersCarousel(),
+        _getSection(AppStrings.services),
+        _getServices(),
+        _getSection(AppStrings.stores),
+        _getStores()
+      ],
+    );
+  }
+
+  Widget _getSection(String title) {
+    return Padding(
+      padding: EdgeInsets.only(
+          top: AppPadding.p12,
+          left: AppPadding.p12,
+          right: AppPadding.p12,
+          bottom: AppPadding.p2),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline3,
+      ),
+    );
+  }
+
+  Widget _getBannersCarousel() {
+    return Center();
+  }
+
+  Widget _getServices() {
+    return Center();
+  }
+
+  Widget _getStores() {
+    return Center();
   }
 
   @override
