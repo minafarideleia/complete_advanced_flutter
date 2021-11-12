64683477325bcf9b42e38dd0bba83479c5ede7ac
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-12 03:55:10 +0300

    Lecture 160 Testing Home Implementation and Fixing Issues

--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -38,7 +38,9 @@ class AppSize {
   static const double s40 = 40.0;
   static const double s60 = 60.0;
   static const double s65 = 65.0;
+  static const double s90 = 90.0;
   static const double s100 = 100.0;
+  static const double s120 = 120.0;
   static const double s130 = 130.0;
   static const double s140 = 140.0;
   static const double s180 = 180.0;
