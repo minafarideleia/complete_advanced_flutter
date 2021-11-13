f941bd8fcc066c34374fb8633c6638faabb7aa4d
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-13 14:57:00 +0300

    Lecture 176 Update Application Class and Settings Screen with localisation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
+import 'package:complete_advanced_flutter/app/app_prefs.dart';
+import 'package:complete_advanced_flutter/app/di.dart';
 import 'package:complete_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class MyApp extends StatefulWidget {
   MyApp._internal(); // private named constructor
@@ -15,9 +18,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {context.setLocale(local)});
+
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
