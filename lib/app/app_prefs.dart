597eddcbf70c557779abb9462a37375c4e696090
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-13 03:25:39 +0300

    Lecture 170 Finalising Setting Screen

--- lib/app/app_prefs.dart
+++ lib/app/app_prefs.dart
@@ -35,4 +35,8 @@ class AppPreferences {
   Future<bool> isUserLoggedIn() async {
     return _sharedPreferences.getBool(PREFS_KEY_IS_USER_LOGGED_IN) ?? false;
   }
+
+  Future<void> logout() async{
+    _sharedPreferences.remove(PREFS_KEY_IS_USER_LOGGED_IN);
+  }
 }
