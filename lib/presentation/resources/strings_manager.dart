840fdc8f57b88a2098324c4e8db3a66108404223
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-13 04:13:09 +0300

    Lecture 171 Start Implementing Localisation

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,5 +1,5 @@
 class AppStrings {
-  static const String noRouteFound = "No Route Found";
+  static const String noRouteFound = "no_route_found";
   static const String onBoardingTitle1 = "SEE THE BEST COURSE #1";
   static const String onBoardingTitle2 = "SEE THE BEST COURSE #2";
   static const String onBoardingTitle3 = "SEE THE BEST COURSE #3";
