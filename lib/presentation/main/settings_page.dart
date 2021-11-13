2bcae0942745dad9bee73dab04c019d0bc349dd7
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-13 03:07:25 +0300

    Lecture 169 Adding UI of Settings Screen

--- lib/presentation/main/settings_page.dart
+++ lib/presentation/main/settings_page.dart
@@ -1,5 +1,8 @@
+import 'package:complete_advanced_flutter/presentation/resources/assets_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -11,6 +14,46 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(AppStrings.settings),);
+    return ListView(
+      padding: EdgeInsets.all(AppPadding.p8),
+      children: [
+        ListTile(
+          title: Text(
+            AppStrings.changeLanguage,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.changeLangIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.contactUs,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.contactUsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.inviteYourFriends,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.logout,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.logoutIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        )
+      ],
+    );
   }
 }
--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -49,8 +49,10 @@ ThemeData getApplicationTheme() {
               color: ColorManager.darkGrey, fontSize: FontSize.s16),
           headline2: getRegularStyle(
               color: ColorManager.white, fontSize: FontSize.s16),
-          headline3: getBoldStyle(
-              color: ColorManager.primary, fontSize: FontSize.s16),
+          headline3:
+              getBoldStyle(color: ColorManager.primary, fontSize: FontSize.s16),
+          headline4: getRegularStyle(
+              color: ColorManager.primary, fontSize: FontSize.s14),
           subtitle1: getMediumStyle(
               color: ColorManager.lightGrey, fontSize: FontSize.s14),
           subtitle2: getMediumStyle(
