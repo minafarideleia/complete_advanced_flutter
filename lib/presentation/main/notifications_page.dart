f9e7100b19e31623a627446400a40592ab46804b
mFaridEbrahim <56106985+mFaridEbrahim@users.noreply.github.com>
2021-11-13 15:19:46 +0300

    Lecture 177 Testing Localisation

--- lib/presentation/main/notifications_page.dart
+++ lib/presentation/main/notifications_page.dart
@@ -1,5 +1,6 @@
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -11,6 +12,8 @@ class NotificationsPage extends StatefulWidget {
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(AppStrings.notifications),);
+    return Center(
+      child: Text(AppStrings.notifications).tr(),
+    );
   }
 }
